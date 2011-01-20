f97324763cc2f1d064d2da2025a5e279d20137fc
Adriano Mitre <adriano.mitre@gmail.com>
2011-01-20 05:10:56 -0200

    initial commit

---
+++ Rakefile
@@ -0,0 +1,38 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+
+# Hoe.plugin :compiler
+# Hoe.plugin :cucumberfeatures
+# Hoe.plugin :gem_prelude_sucks
+# Hoe.plugin :inline
+# Hoe.plugin :manifest
+# Hoe.plugin :newgem
+# Hoe.plugin :racc
+# Hoe.plugin :rubyforge
+# Hoe.plugin :website
+
+Hoe.spec 'retroactive_module_inclusion' do
+  # HEY! If you fill these out in ~/.hoe_template/Rakefile.erb then
+  # you'll never have to touch them again!
+  # (delete this comment too, of course)
+
+  developer('Adriano Mitre', 'adriano.mitre@gmail.com')
+  
+  self.version = '1.0.0'
+
+  self.readme_file = 'README.rdoc'
+  self.history_file = 'History.rdoc'
+  self.extra_rdoc_files += ['README.rdoc', 'History.rdoc']
+  self.extra_rdoc_files << ['Wishlist.rdoc'] if File.exist? 'Wishlist.rdoc'
+
+  # self.rubyforge_name = 'retroactive_module_inclusionx' # if different than 'retroactive_module_inclusion'
+end
+
+# vim: syntax=ruby
+
+task :tests => [:test] do
+  # aliasing :test with :tests for RVM ('rvm tests')
+end
+
---
+++ test/test_retroactive_module_inclusion.rb
@@ -0,0 +1,46 @@
+require "test/unit"
+require File.expand_path("../../lib/retroactive_module_inclusion", __FILE__)
+
+module Stats
+  def mean
+    n = 1
+    inject {|s,k| n += 1 ; s + k }.to_f / count
+  end
+end
+
+class TestRetroactiveInclude < Test::Unit::TestCase
+
+  def test_argument_type
+    type_error(Enumerable, Array)
+    type_error(Comparable, Range)
+    type_error(Math, Numeric)
+    type_ok(Array, Enumerable)
+    type_ok(Range, Comparable)
+    type_ok(Numeric, Math)
+  end
+  
+  def test_retroactively_include
+    assert_raise(NoMethodError) { (1..2).mean }
+    Enumerable.module_eval { include Stats }
+    assert_raise(NoMethodError, 'include should not work retroactively ') { (1..2).mean }
+    Enumerable.module_eval { retroactively_include Stats }
+    assert_nothing_raised('retroactively_include should do the job') { (1..2).mean }
+    assert_equal 1.5, (1..2).mean
+  end
+  
+  private
+  
+  def type_error(a, b)
+    assert_raise(TypeError, 'shold raise TypeError for non Module inclusion') do
+      a.module_eval { retroactively_include b }
+    end
+  end
+  
+  def type_ok(a, b)
+    assert_nothing_raised('shold not raise Exception for Module inclusion') do
+      a.module_eval { retroactively_include b }
+    end
+  end
+  
+end
+
